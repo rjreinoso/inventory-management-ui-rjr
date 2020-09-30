89aaa87f81f9dbf2f43e12028585762abe4d3018
Ramiro Reinoso <ramiro.reinoso.ibm@Ramiros-MBP.fios-router.home>
2020-09-30 14:30:01 -0400

    Updates the StockItemsList view

---
+++ client/src/services/stock-item.service.js
@@ -0,0 +1,23 @@
+export class StockItemService {
+    async listStockItems() {
+      return [];
+    }
+  }
+
+import * as superagent from 'superagent';
+
+export class StockItemService {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl || '/api';
+  }
+
+  async listStockItems() {
+    return superagent
+      .get(this.baseUrl + '/stock-items')
+      .set('accept', 'application/json')
+      .then(res => {
+        console.log('Got response: ', res);
+        return res.body || [];
+      });
+  }
+}
