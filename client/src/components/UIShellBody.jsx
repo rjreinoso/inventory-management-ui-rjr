a88d0fa11c742b62c40a434721fd304ce463add3
Ramiro Reinoso <ramiro.reinoso.ibm@Ramiros-MBP.fios-router.home>
2020-10-01 11:27:05 -0400

    Updates the StockItemsList view

--- client/src/components/UIShellBody.jsx
+++ client/src/components/UIShellBody.jsx
@@ -1,17 +1,4 @@
 import React, { Component } from "react";
-import DisplayForm from "../pattern-components/DisplayForm";
-import ValidatingForm from "../pattern-components/ValidatingForm";
-import UpdateForm from "../pattern-components/UpdateForm";
-import ValidatingFormWizard1 from "../pattern-components/ValidatingFormWizard1";
-import ValidatingFormWizard2 from "../pattern-components/ValidatingFormWizard2";
-import SimpleList from "../pattern-components/SimpleList";
-import TableList from "../pattern-components/TableList";
-import ListToList from "../pattern-components/ListToList";
-import LinkedList from "../pattern-components/LinkedList";
-import MasterDetail from "../pattern-components/MasterDetail";
-import CreateReadUpdateDelete from "../pattern-components/CreateReadUpdateDelete";
-import SearchList from "../pattern-components/SearchList";
-import SearchForm from "../pattern-components/SearchForm";
 import "../pattern-components/patterns.scss";
 
 import StockItemList from "./StockItemList";
